4405fb8744b8689782c2c6fea7f6767f7b01f0a2
Trever Faden <trever@users.noreply.github.com>
2015-06-23 19:26:40 -0400

    Deps

--- node_modules/meteor-random/test.js
+++
@@ -1,6 +0,0 @@
-var Random = require('./index.js');
-console.log('test test test');
-//exports.printMsg = function() {
-  console.log("This is a message from the demo package");
-  console.log(Random.secret());
-//};
